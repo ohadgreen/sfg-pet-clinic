c22ad5f43a395f0c9733bfea747c734b3e9e8aa5
ohadgreen <ohad.green@gmail.com>
2019-05-07 18:58:27 +0300

    vets controller

---
+++ pet-clinic-web/src/main/java/com/acme/sfgpetclinic/controllers/VetsController.java
@@ -0,0 +1,13 @@
+package com.acme.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetsController {
+
+    @RequestMapping({"/vets", "/vets/index", "vets/index.html"})
+    public String vetsList() {
+        return "vets/index";
+    }
+}
