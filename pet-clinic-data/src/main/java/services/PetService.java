88360b4a80411e94d10e8a701e0aa7d847d12862
ohadgreen <ohad.green@gmail.com>
2019-05-07 13:35:57 +0300

    refactor services interface to use common crud

--- pet-clinic-data/src/main/java/services/PetService.java
+++ pet-clinic-data/src/main/java/services/PetService.java
@@ -1,13 +1,9 @@
 package services;
 
+import model.Owner;
 import model.Pet;
 
 import java.util.Set;
 
-public interface PetService {
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long>{
 }
--- pet-clinic-data/src/main/java/services/VetService.java
+++ pet-clinic-data/src/main/java/services/VetService.java
@@ -1,14 +1,6 @@
 package services;
 
-import model.Pet;
 import model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-    Vet findById(Long id);
-
-    Vet save(Pet vet);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService <Vet, Long>{
 }
