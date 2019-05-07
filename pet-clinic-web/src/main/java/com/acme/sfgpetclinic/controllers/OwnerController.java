b436cec7ccc51d6f15dd7a7fb60598dd584e9903
ohadgreen <ohad.green@gmail.com>
2019-05-07 19:05:45 +0300

    owners controller and index page

---
+++ pet-clinic-web/src/main/java/com/acme/sfgpetclinic/controllers/OwnerController.java
@@ -0,0 +1,12 @@
+package com.acme.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class OwnerController {
+    @RequestMapping({"/owners", "/owners/index", "owners/index.html"})
+    public String ownersList() {
+        return "owners/index";
+    }
+}
