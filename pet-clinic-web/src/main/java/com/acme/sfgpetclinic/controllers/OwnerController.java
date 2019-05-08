4169731445cd556bbfea9bdf8bebb8072b9821d3
ohadgreen <ohad.green@gmail.com>
2019-05-08 15:47:11 +0300

    owners list display

--- pet-clinic-web/src/main/java/com/acme/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/acme/sfgpetclinic/controllers/OwnerController.java
@@ -1,12 +1,29 @@
 package com.acme.sfgpetclinic.controllers;
 
+import com.acme.sfgpetclinic.model.Owner;
+import com.acme.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Set;
 
 @Controller
 public class OwnerController {
+
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"/owners", "/owners/index", "owners/index.html"})
-    public String ownersList() {
+    public String ownersList(Model model) {
+        Set<Owner> allOwners = ownerService.findAll();
+        System.out.println("owners list: \n =======");
+        allOwners.forEach(o -> System.out.println(o.getFirstName()));
+
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
