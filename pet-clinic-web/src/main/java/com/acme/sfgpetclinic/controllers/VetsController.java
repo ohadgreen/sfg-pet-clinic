7d0fd3a51b727edfd51f3df2567fe1db961b4ed6
ohadgreen <ohad.green@gmail.com>
2019-05-08 15:57:14 +0300

    vets list display

--- pet-clinic-web/src/main/java/com/acme/sfgpetclinic/controllers/VetsController.java
+++ pet-clinic-web/src/main/java/com/acme/sfgpetclinic/controllers/VetsController.java
@@ -1,13 +1,23 @@
 package com.acme.sfgpetclinic.controllers;
 
+import com.acme.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetsController {
 
+    private final VetService vetService;
+
+    public VetsController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets", "/vets/index", "vets/index.html"})
-    public String vetsList() {
+    public String vetsList(Model model) {
+        model.addAttribute("vets", vetService.findAll());
+
         return "vets/index";
     }
 }
