751c5e6cf09fbb01f7927f729b17c6ce494ba84d
ohadgreen <ohad.green@gmail.com>
2019-05-15 09:53:46 +0300

    PetType map service

--- pet-clinic-data/src/main/java/com/acme/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/acme/sfgpetclinic/model/Vet.java
@@ -1,7 +1,19 @@
 package com.acme.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
+    private Set<Specialty> specialties;
+
     public Vet(String firstName, String lastName) {
         super(firstName, lastName);
+    }
+
+    public Set<Specialty> getSpecialties() {
+        return specialties;
+    }
+
+    public void setSpecialties(Set<Specialty> specialties) {
+        this.specialties = specialties;
     }
 }
---
+++ pet-clinic-data/src/main/java/com/acme/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.acme.sfgpetclinic.services;
+
+import com.acme.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long>{
+}
